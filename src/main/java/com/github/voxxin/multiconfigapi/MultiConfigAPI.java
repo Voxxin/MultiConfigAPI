0a9985f9d20a6510cebe8892c8f98c313f851a45
Voxxin <59192970+Voxxin@users.noreply.github.com>
2024-02-29 14:32:45 -0500

    Init

---
+++ src/main/java/com/github/voxxin/multiconfigapi/MultiConfigAPI.java
@@ -0,0 +1,11 @@
+package com.github.voxxin.multiconfigapi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class MultiConfigAPI implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
