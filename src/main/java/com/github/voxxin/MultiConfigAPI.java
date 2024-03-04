1deef81e3329eefb2c3ecce020b10e5a4695be37
Voxxin <59192970+Voxxin@users.noreply.github.com>
2024-03-03 19:50:42 -0500

    forgor debug

--- src/main/java/com/github/voxxin/MultiConfigAPI.java
+++ src/main/java/com/github/voxxin/MultiConfigAPI.java
@@ -25,33 +25,33 @@ public class MultiConfigAPI implements ClientModInitializer {
          * runOptions(); (This will either, create, or load & update the options.)
          */
 
-        ConfigManager manager = new ConfigManager(MODID);
-        BooleanConfigOption toggleCape = new BooleanConfigOption("multiconfigapi.general.boolean", false);
-        StringConfigOption whateverString = new StringConfigOption("multiconfigapi.general.string", "this should not appear");
-        SliderConfigOption soundSlider = new SliderConfigOption("multiconfigapi.general.slider", 0, 2, 1);
-        ArrayConfigOption<String> array = new ArrayConfigOption<>("multiconfigapi.general.array");
-        array.addElement("minecraft:true");
-
-        CycleConfigOption<String> cycle = new CycleConfigOption<>("multiconfig.general.cycle");
-        cycle.addElement("Test 000");
-        cycle.addElement("Test 123");
-        cycle.addElement("Test 456");
-        cycle.addElement("Test 789");
-
-        ConfigOption subOption = new ConfigOption.Builder("another.option")
-                .addBoolean(new BooleanConfigOption("test", true))
-                .build();
-
-        ConfigOption configOption = new ConfigOption.Builder("awesome.category")
-                .addBoolean(toggleCape)
-                .addString(whateverString)
-                .addSlider(soundSlider)
-                .addArray(array)
-                .addCycle(cycle)
-                .addConfigOption(subOption)
-                .build();
-
-        manager.addOption(configOption);
-        manager.runOptions();
+//        ConfigManager manager = new ConfigManager(MODID);
+//        BooleanConfigOption toggleCape = new BooleanConfigOption("multiconfigapi.general.boolean", false);
+//        StringConfigOption whateverString = new StringConfigOption("multiconfigapi.general.string", "this should not appear");
+//        SliderConfigOption soundSlider = new SliderConfigOption("multiconfigapi.general.slider", 0, 2, 1);
+//        ArrayConfigOption<String> array = new ArrayConfigOption<>("multiconfigapi.general.array");
+//        array.addElement("minecraft:true");
+//
+//        CycleConfigOption<String> cycle = new CycleConfigOption<>("multiconfig.general.cycle");
+//        cycle.addElement("Test 000");
+//        cycle.addElement("Test 123");
+//        cycle.addElement("Test 456");
+//        cycle.addElement("Test 789");
+//
+//        ConfigOption subOption = new ConfigOption.Builder("another.option")
+//                .addBoolean(new BooleanConfigOption("test", true))
+//                .build();
+//
+//        ConfigOption configOption = new ConfigOption.Builder("awesome.category")
+//                .addBoolean(toggleCape)
+//                .addString(whateverString)
+//                .addSlider(soundSlider)
+//                .addArray(array)
+//                .addCycle(cycle)
+//                .addConfigOption(subOption)
+//                .build();
+//
+//        manager.addOption(configOption);
+//        manager.runOptions();
     }
 }
